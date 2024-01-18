9cfd2c2c23dbc213e4a156b35ac15cf458b9401a
ramiferrot18 <147268248+ramiferrot18@users.noreply.github.com>
2024-01-18 09:45:25 +0000

    app-2

--- app-2/src/components/Timer.js
+++ app-2/src/components/Timer.js
@@ -1,18 +1,33 @@
-
 import "../style/Timer.css";
+import { useState } from "react";
 
-function Timer(startTimer, stopTimer, resetTimer) {
-    return (
-        <div className="container">
-        <h1>Timer</h1>
-        <span>{Math.trunc(Timer / 60)} mins </span>
-        <span>{Timer % 60} secs</span>
-        <div>
-            <button onClick={() => startTimer()}>Start</button>
-            <button onClick={() => stopTimer()}>Stop</button>
-            <button onClick={() => resetTimer()}>Reset</button>
-        </div>
-        </div>
-    );
+function Timer() {
+  const [timer, setTimer] = useState(0);
+
+  const startTimer = () => {
+    window.myTimer = setInterval(() => {
+      setTimer((timer) => timer + 1);
+    }, 1000);
+  };
+  const stopTimer = () => {
+    clearInterval(window.myTimer);
+  };
+  const resetTimer = () => {
+    clearInterval(window.myTimer);
+    setTimer(0);
+  };
+  return (
+    <div className="container">
+      <h1>Timer</h1>
+      <span>{Math.trunc(timer / 60)} mins </span>
+      <span>{timer % 60} secs</span>
+      <div>
+        <button onClick={startTimer}>Start</button>
+        <button onClick={stopTimer}>Stop</button>
+        <button onClick={resetTimer}>Reset</button>
+      </div>
+    </div>
+  );
 }
+
 export default Timer;
